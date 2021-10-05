7fe281e7fa8656a23e0313a049d0e0c1ac4d2806
Rendles <Rendles>
2021-10-05 17:22:15 +0700

    try blade

--- gallery.php
+++ gallery.php
@@ -1,14 +1,21 @@
+
+
 <!DOCTYPE HTML>
 <html>
 
 <head>
-    
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="Styles.css">
+    <title>Клуб любителей Шрека!</title>
 </head>
 
 <body>
+
     <header> 
     </header>
+
  <p>FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF</p>
+ 
     <footer> 
     </footer>
 
---
+++ header.blade.php
@@ -0,0 +1,12 @@
+<div style="display: grid; grid-template-columns: 40% 60%;">
+    <a href="/" style='color: white; text-decoration: none; '>
+        <img src="images/HeaderName.png"></img>
+    </a>
+    <div>
+        <a href="gallery.php" class="floating-button">Галерея</a>
+        <a href="/747_srs/page404.html" class="floating-button">Шрек</a>
+        <a href="/747_srs/page404.html" class="floating-button">Шрек 2</a>
+        <a href="/747_srs/page404.html" class="floating-button">Шрек Третий</a>
+        <a href="/747_srs/page404.html" class="floating-button">Шрек навсегда</a>
+    </div>
+</div>
