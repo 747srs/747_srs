738f3ddd9d25371af68841cf58b3f31e720cae65
Rendles <Rendles>
2021-10-07 15:57:11 +0700

    1st page on php

--- gallery.php
+++ gallery.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gallery</title>
 </head>
 <body>
-    <div align="center">
+    <div align="center" style:"font-size: 200%">
     <?php
-    echo 'hello its me, Mario';
+    function foo($hello){
+        echo rand() . "\n"
+
+    }
+    echo foo();
+
+
+
+
     ?>
     </div>
 </body>
