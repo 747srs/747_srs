5dd144399a5f5a1939663e75108e2eaae18d4aa7
Rendles <Rendles>
2021-10-07 13:49:06 +0700

    1st page on php

--- gallery.php
+++ gallery.php
@@ -1,24 +1,2 @@
-
-
-<!DOCTYPE HTML>
-<html>
-
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" href="Styles.css">
-    <title>Клуб любителей Шрека!</title>
-</head>
-
-<body>
-
-    <header> 
-    </header>
-
- <p>FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF</p>
- 
-    <footer> 
-    </footer>
-
-</body>
-
-</html>
+<?php
+echo "<h1>Second page on php</h1>"
